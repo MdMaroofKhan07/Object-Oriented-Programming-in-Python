8b264c145a3ba7a186cdd62d0a1dd1319fcd8960
Md Maroof Khan <kmaroof1107@gmail.com>
2026-08-28 21:41:59 +0530

    Add files via upload

---
+++ Lab 1/Without__init__.py
@@ -0,0 +1,7 @@
+# Without the__init__()method, you would need to set properties manually for each object
+class Person: pass
+p1 = Person()
+p1.name ="Tobias"
+p1.age =25
+print(p1.name)
+print(p1.age)
